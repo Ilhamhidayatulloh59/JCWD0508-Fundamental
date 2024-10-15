a222bbdd29f36015c9e833c7c69334852b9b6d85
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-10-15 16:05:31 +0700

    exercise day-11

--- review.ts
+++ review.ts
@@ -1,10 +1,37 @@
-// const arr: number[] = [11, 21, 22, 5, 30, 14]
+const data: any[] = [
+    () => [
+        ["andi", "budi"],
+        false
+    ],
+    [() => [10, 11, 12], 13],
+    { num: [1, 2, 3], count(){ return "Hello World" } },
+    () => () => {
+        return {
+            great() {
+                return "Purwadhika Bandung"
+            }
+        }
+    }
+]
 
-// const arrAsc: number[] = arr.sort((a, b) => a - b)
-// const arrDesc: number[] = arr.sort((a, b) => b - a)
+// console.log(data[3]()()["great"]())
+// console.log(data[2].count())
+// console.log(data[2].num)
+// console.log(Object.keys(data[2]))
+// console.log(data.length)
+// console.log(data[0]()[1])
+// console.log(data[1][0]()[1])
+// console.log(data[1][1])
 
-// arrDesc.splice(0, 3)
+const obj: {[ x: string]: any } = {
+    name: "andi",
+    age: 21,
+    country: "indonesia",
+    1: "budi"
+}
 
-// console.log(arr)
-// console.log(arrAsc)
-// console.log(arrDesc)
+console.log(obj)
+
+// for (let key in obj) {
+//     console.log(obj[key])
+// }
---
+++ xor.ts
@@ -0,0 +1,6 @@
+const a = 3 // 0011
+const b = 4 // 0100
+
+const res = a ^ b // 0111 = 7
+
+console.log(res)
